53be3e1ed550b41e3ebe8ef1d440ca066d1a9934
Shashank Kafle <shashank.kaphle@gmail.com>
2026-06-14 11:55:47 +0700

    change config for 3 lane

--- configurations.py
+++ configurations.py
@@ -1,50 +1,50 @@
 # # # configurations.py 3 lane roads
 
-# location_dict = {"N": "North", "S": "South", "E": "East", "W": "West"}
-# location_dict_detail = {"N": "Northern", "S": "Southern", "E": "Eastern", "W": "Western"}
+location_dict = {"N": "North", "S": "South", "E": "East", "W": "West"}
+location_dict_detail = {"N": "Northern", "S": "Southern", "E": "Eastern", "W": "Western"}
 
-# incoming_lane_2_outgoing_road = {
-#     "NT": "South", "NL": "East",
-#     "ST": "North", "SL": "West",
-#     "ET": "West",  "EL": "South",
-#     "WT": "East",  "WL": "North"
-# }
+incoming_lane_2_outgoing_road = {
+    "NT": "South", "NL": "East",
+    "ST": "North", "SL": "West",
+    "ET": "West",  "EL": "South",
+    "WT": "East",  "WL": "North"
+}
 
-# # The centralized configuration dictionary
-# INTERSECTION_CONFIG = {
-#     "global_settings": {
-#         "all_red_state": "gggrrrrrrgggrrrrrrgggrrrrrrgggrrrrrr",
-#         "default_green_duration": 30,
-#         "yellow_duration": 3,
-#         "red_duration": 2
-#     },
-#     "phases": {
-#         "ETWT": {
-#             "id": 0,
-#             "green": "gggrrrrrrgggGGGrrrgggrrrrrrgggGGGrrr",
-#             "yellow": "gggrrrrrrgggyyyrrrgggrrrrrrgggyyyrrr",
-#             "llm_description": "- ETWT: Eastern and western through lanes."
-#         },
-#         "NTST": {
-#             "id": 1,
-#             "green": "gggGGGrrrgggrrrrrrgggGGGrrrgggrrrrrr",
-#             "yellow": "gggyyyrrrgggrrrrrrgggyyyrrrgggrrrrrr",
-#             "llm_description": "- NTST: Northern and southern through lanes."
-#         },
-#         "ELWL": {
-#             "id": 2,
-#             "green": "gggrrrrrrgggrrrGGGgggrrrrrrgggrrrGGG",
-#             "yellow": "gggrrrrrrgggrrryyygggrrrrrrgggrrryyy",
-#             "llm_description": "- ELWL: Eastern and western left-turn lanes."
-#         },
-#         "NLSL": {
-#             "id": 3,
-#             "green": "gggrrrGGGgggrrrrrrgggrrrGGGgggrrrrrr",
-#             "yellow": "gggrrryyygggrrrrrrgggrrryyygggrrrrrr",
-#             "llm_description": "- NLSL: Northern and southern left-turn lanes."
-#         }
-#     }
-# }
+# The centralized configuration dictionary
+INTERSECTION_CONFIG = {
+    "global_settings": {
+        "all_red_state": "gggrrrrrrgggrrrrrrgggrrrrrrgggrrrrrr",
+        "default_green_duration": 30,
+        "yellow_duration": 3,
+        "red_duration": 2
+    },
+    "phases": {
+        "ETWT": {
+            "id": 0,
+            "green": "gggrrrrrrgggGGGrrrgggrrrrrrgggGGGrrr",
+            "yellow": "gggrrrrrrgggyyyrrrgggrrrrrrgggyyyrrr",
+            "llm_description": "- ETWT: Eastern and western through lanes."
+        },
+        "NTST": {
+            "id": 1,
+            "green": "gggGGGrrrgggrrrrrrgggGGGrrrgggrrrrrr",
+            "yellow": "gggyyyrrrgggrrrrrrgggyyyrrrgggrrrrrr",
+            "llm_description": "- NTST: Northern and southern through lanes."
+        },
+        "ELWL": {
+            "id": 2,
+            "green": "gggrrrrrrgggrrrGGGgggrrrrrrgggrrrGGG",
+            "yellow": "gggrrrrrrgggrrryyygggrrrrrrgggrrryyy",
+            "llm_description": "- ELWL: Eastern and western left-turn lanes."
+        },
+        "NLSL": {
+            "id": 3,
+            "green": "gggrrrGGGgggrrrrrrgggrrrGGGgggrrrrrr",
+            "yellow": "gggrrryyygggrrrrrrgggrrryyygggrrrrrr",
+            "llm_description": "- NLSL: Northern and southern left-turn lanes."
+        }
+    }
+}
 
 
 
@@ -74,77 +74,77 @@
 #   8,9  -> ST  (South Through)
 #  10,11 -> SL  (South Left-turn)
 #  12,13 -> WT  (West  Through)
-#  14,15 -> WL  (West  Left-turn)
+# #  14,15 -> WL  (West  Left-turn)
 
-location_dict = {"N": "North", "S": "South", "E": "East", "W": "West"}
-location_dict_detail = {"N": "Northern", "S": "Southern", "E": "Eastern", "W": "Western"}
+# location_dict = {"N": "North", "S": "South", "E": "East", "W": "West"}
+# location_dict_detail = {"N": "Northern", "S": "Southern", "E": "Eastern", "W": "Western"}
 
-# Maps a movement code to the road that vehicles are heading toward
-incoming_lane_2_outgoing_road = {
-    "NT": "South", "NL": "East",
-    "ST": "North", "SL": "West",
-    "ET": "West",  "EL": "South",
-    "WT": "East",  "WL": "North"
-}
-
-
+# # Maps a movement code to the road that vehicles are heading toward
+# incoming_lane_2_outgoing_road = {
+#     "NT": "South", "NL": "East",
+#     "ST": "North", "SL": "West",
+#     "ET": "West",  "EL": "South",
+#     "WT": "East",  "WL": "North"
+# }
 
 
 
-# The centralized configuration dictionary
-INTERSECTION_CONFIG = {
-    "global_settings": {
-        "intersection_id":    "intersection_1_1",
-        "all_red_state":      "rrrrrrrrrrrrrrrr",
-        "default_green_duration": 30,
-        "yellow_duration":     3,
-        "red_duration":        2,
-    },
 
-    "phases": {
-        "NTST": {
-            "id": 0,
-            "green":  "GGrrrrrrGGrrrrrr",
-            "yellow": "yyrrrrrryyrrrrrr",
-            "llm_description": "- NTST: Northern and southern through lanes.",
-        },
-        "ETWT": {
-            "id": 1,
-            "green":  "rrrrGGrrrrrrGGrr",
-            "yellow": "rrrryyrrrrrryyrr",
-            "llm_description": "- ETWT: Eastern and western through lanes.",
-        },
-        "NLSL": {
-            "id": 2,
-            "green":  "rrGGrrrrrrGGrrrr",
-            "yellow": "rryyrrrrrryyrrrr",
-            "llm_description": "- NLSL: Northern and southern left-turn lanes.",
-        },
-        "ELWL": {
-            "id": 3,
-            "green":  "rrrrrrGGrrrrrrGG",
-            "yellow": "rrrrrryyrrrrrryy",
-            "llm_description": "- ELWL: Eastern and western left-turn lanes.",
-        },
-    },
 
-        "movement_directions": {
-        "NT": "North", "NL": "North",
-        "ST": "South", "SL": "South",
-        "ET": "East",  "EL": "East",
-        "WT": "West",  "WL": "West",
-    },
+# # The centralized configuration dictionary
+# INTERSECTION_CONFIG = {
+#     "global_settings": {
+#         "intersection_id":    "intersection_1_1",
+#         "all_red_state":      "rrrrrrrrrrrrrrrr",
+#         "default_green_duration": 30,
+#         "yellow_duration":     3,
+#         "red_duration":        2,
+#     },
 
-    "movement_types": {
-        "NT": "through", "ST": "through", "ET": "through", "WT": "through",
-        "NL": "left-turn", "SL": "left-turn", "EL": "left-turn", "WL": "left-turn",
-    },
+#     "phases": {
+#         "NTST": {
+#             "id": 0,
+#             "green":  "GGrrrrrrGGrrrrrr",
+#             "yellow": "yyrrrrrryyrrrrrr",
+#             "llm_description": "- NTST: Northern and southern through lanes.",
+#         },
+#         "ETWT": {
+#             "id": 1,
+#             "green":  "rrrrGGrrrrrrGGrr",
+#             "yellow": "rrrryyrrrrrryyrr",
+#             "llm_description": "- ETWT: Eastern and western through lanes.",
+#         },
+#         "NLSL": {
+#             "id": 2,
+#             "green":  "rrGGrrrrrrGGrrrr",
+#             "yellow": "rryyrrrrrryyrrrr",
+#             "llm_description": "- NLSL: Northern and southern left-turn lanes.",
+#         },
+#         "ELWL": {
+#             "id": 3,
+#             "green":  "rrrrrrGGrrrrrrGG",
+#             "yellow": "rrrrrryyrrrrrryy",
+#             "llm_description": "- ELWL: Eastern and western left-turn lanes.",
+#         },
+#     },
 
-    "movement_outgoing_road": {
-        "NT": "South", "NL": "East",
-        "ST": "North", "SL": "West",
-        "ET": "West",  "EL": "South",
-        "WT": "East",  "WL": "North",
-    },
+#         "movement_directions": {
+#         "NT": "North", "NL": "North",
+#         "ST": "South", "SL": "South",
+#         "ET": "East",  "EL": "East",
+#         "WT": "West",  "WL": "West",
+#     },
 
-}
+#     "movement_types": {
+#         "NT": "through", "ST": "through", "ET": "through", "WT": "through",
+#         "NL": "left-turn", "SL": "left-turn", "EL": "left-turn", "WL": "left-turn",
+#     },
+
+#     "movement_outgoing_road": {
+#         "NT": "South", "NL": "East",
+#         "ST": "North", "SL": "West",
+#         "ET": "West",  "EL": "South",
+#         "WT": "East",  "WL": "North",
+#     },
+
+# }
